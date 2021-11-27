af7a5efb34099faa95568b12e5c4c457e6e353ea
Navneet <navn0306@gmail.com>
2021-11-28 01:13:17 +0530

    ADD: Added welcome message for proram

---
+++ src/com/bridge/SwapNumbers/SwapNumbers.java
@@ -0,0 +1,7 @@
+package com.bridge.SwapNumbers;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, This program swaps the entered number.");
+    }
+}
